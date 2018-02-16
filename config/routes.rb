89e8b19e42be4dbf15eaf50ddb9436271fe62f05
alex0112 <alarsen0112@gmail.com>
2018-02-15 20:38:58 -0700

    Forgot the routes file.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'comments#index'
+
+  resources :comments
+  resources :students
+  resources :judges
+  resources :tournaments
 end
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
